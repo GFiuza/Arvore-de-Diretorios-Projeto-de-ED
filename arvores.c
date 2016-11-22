b8988c7632d6760b9edccde46c8e64554ac9d13b
GFiuza <gabriel_fiuza@hotmail.com>
2016-11-22 10:31:09 -0200

    Update arvores.c

--- arvores.c
+++ arvores.c
@@ -5,7 +5,7 @@
 typedef struct arvdir{
     struct arvdir *pai, *filho, *irmao;
     char[] nome, dat_criacao, dat_atualiza, permissoes;
-    int num_de_dir, num_de_arquivo;
+    int num_de_dir, num_de_arq, arquivo;
 
 }TAD;
 
@@ -20,9 +20,13 @@ TAD * cria (char[] c){
     a.irmao = NULL;
     nome = c;
     
+    time ( &rawtime );
+    timeinfo = localtime ( &rawtime );
     strftime(str, 20, "%d/%m/%Y %H:%M:%S", timeinfo);
+    
     dat_criacao = str;
     dat_atualiza = str;
     num_de_dir = 0;
-    num_de_arquivo = 0;
+    num_de_arq = 0;
+    arq = 0;
 };
