51871b8dcb843ff37a5011ddcae2648593bf9cb6
GFiuza <gabriel_fiuza@hotmail.com>
2016-11-22 10:29:25 -0200

    Create arvores.c

---
+++ arvores.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+
+//definição da estrutura de árvore de diretório
+typedef struct arvdir{
+    struct arvdir *pai, *filho, *irmao;
+    char[] nome, dat_criacao, dat_atualiza, permissoes;
+    int num_de_dir, num_de_arquivo;
+
+}TAD;
+
+//definição dos métodos básicos de árvores
+TAD * cria (char[] c){
+    time_t rawtime;
+    struct tm * timeinfo;
+    char str[20];
+    TAD *a = (TAD*) malloc (sizeof(TAD));
+    a.pai = NULL;
+    a.filho = NULL;
+    a.irmao = NULL;
+    nome = c;
+    
+    strftime(str, 20, "%d/%m/%Y %H:%M:%S", timeinfo);
+    dat_criacao = str;
+    dat_atualiza = str;
+    num_de_dir = 0;
+    num_de_arquivo = 0;
+};
